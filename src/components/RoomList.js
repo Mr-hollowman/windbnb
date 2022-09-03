7c38caf949a708b7491100952866af5e8e4360f0
mr-hollowman <selvamani825@gmail.com>
2022-09-03 22:11:41 +0530

    render the view

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 export default function RoomList({ roomList }) {
-  console.log(roomList);
-  return <div>RoomList</div>;
+  return (
+    <div>
+      {roomList.map((res, j) => (
+        <div key={j}>
+          <div className="img-container">
+            <img src={res.photo} alt="image" />
+          </div>
+          <div>
+            {res.superHost&&<span>SUPER HOST</span>}
+            <span>{res.type}.{res.beds}beds</span>
+            <span>{res.rating}</span>
+          </div>
+          <div>{res.title}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
